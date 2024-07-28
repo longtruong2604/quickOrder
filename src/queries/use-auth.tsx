9049810115787445946e42540d351fbd525fc9b3
Truong Thanh Long <longtruong2604@gmail.com>
2024-07-28 15:13:28 +0700

    feat: add react query

---
+++ src/queries/use-auth.tsx
@@ -0,0 +1,6 @@
+import authApiRequest from "@/apiRequest/auth";
+import { useMutation } from "@tanstack/react-query";
+
+export const useLoginMutation = () => {
+  return useMutation({ mutationFn: authApiRequest.login });
+};
