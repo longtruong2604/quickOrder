6f27d2a0d588f7ea6a74976c563bcf6d7e786cbc
long.tt <long.tt@atomsolution.com.vn>
2024-08-28 22:01:38 +0700

    feat: add update my account feature

--- src/queries/use-account.tsx
+++ src/queries/use-account.tsx
@@ -1,10 +1,23 @@
 import accountApiRequest from "@/apiRequest/account";
 import { AccountResType } from "@/schemaValidations/account.schema";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-export const useAccountQuery = (onSuccess?: (data:AccountResType) => void) => {
+export const useAccountMeQuery = (
+  onSuccess?: (data: AccountResType) => void
+) => {
   return useQuery({
-    queryKey: ["account-profile"],
-    queryFn: accountApiRequest.me,
+    queryKey: ["account-me"],
+    queryFn: () =>
+      accountApiRequest.me().then((res) => {
+        onSuccess && onSuccess(res.payload); //executes the callback function if it exists
+        return res;
+      }),
+  });
+};
+
+export const useUpdateMeMutation = () => {
+  return useMutation({
+    mutationKey: ["update-me"],
+    mutationFn: accountApiRequest.updateMe,
   });
 };
