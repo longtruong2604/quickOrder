bcae780e5c7fa5da77b39bad225e79dd39efbabd
longtruong2604 <longtruong2604@gmail.com>
2024-09-06 01:40:39 +0700

    feat: refresh token after not visiting and token expired

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,8 @@ import { UseFormSetError } from 'react-hook-form'
 import { twMerge } from 'tailwind-merge'
 import { EntityError } from './http'
 import { toast } from '@/components/ui/use-toast'
+import jwt from 'jsonwebtoken'
+import authApiRequest from '@/apiRequest/auth'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -50,3 +52,36 @@ export const setAccessTokenToLocalStorage = (accessToken: string) =>
 
 export const setRefreshTokenToLocalStorage = (refreshToken: string) =>
   isBrowser ? localStorage.setItem('refreshToken', refreshToken) : null
+
+export const checkAndRefreshToken = async (param?: {
+  onError?: (error: any) => void
+  onSuccess?: () => void
+}) => {
+  const accessToken = getAccessTokenFromLocalStorage()
+  const refreshToken = getRefreshTokenFromLocalStorage()
+  if (!accessToken || !refreshToken) return
+  const decodedAccessToken = jwt.decode(accessToken) as {
+    exp: number
+    iat: number
+  }
+  const decodedRefreshToken = jwt.decode(refreshToken) as {
+    exp: number
+    iat: number
+  }
+  const now = Math.round(new Date().getTime() / 1000)
+  if (decodedRefreshToken.exp <= now) return
+  if (
+    decodedAccessToken.exp - now <
+    (decodedAccessToken.exp - decodedAccessToken.iat) / 3
+  ) {
+    // Gọi API refresh token
+    try {
+      const res = await authApiRequest.refreshToken() //logout automatically because of http error 401
+      setAccessTokenToLocalStorage(res.payload.data.accessToken)
+      setRefreshTokenToLocalStorage(res.payload.data.refreshToken)
+      param?.onSuccess && param.onSuccess()
+    } catch (error) {
+      param?.onError && param.onError(error)
+    }
+  }
+}
