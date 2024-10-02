b462dafc9fccd6d3aa3c48f476532323610332b6
longtruong2604 <longtruong2604@gmail.com>
2024-10-02 23:51:36 +0700

    feat: get dishes on home page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000',
+      },
+      {
+        protocol: 'https',
+        hostname: 'longtruong-uploads.s3.ap-southeast-2.amazonaws.com',
+      },
+    ],
+  },
+}
 
 export default nextConfig
